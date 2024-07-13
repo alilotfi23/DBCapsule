6500fab82477daeef875d10c0eedb42d45d4b084
Ali Lotfi <91953142+alilotfi23@users.noreply.github.com>
2024-07-13 19:41:22 +0330

    add --schema-only and --tablespace

--- DBCapsule/backup.py
+++ DBCapsule/backup.py
@@ -5,27 +5,42 @@ from datetime import datetime
 import typer
 
 
-def backup_database(db_type: str, username: str, password: str, db_name: Optional[str], path: Path):
+def backup_database(
+        db_type: str,
+        username: str,
+        password: str,
+        db_name: Optional[str],
+        path: Path,
+        tablespaces: bool,
+        schema: bool,
+):
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
     if db_type == "mysql":
         if db_name == "all":
             backup_file = path / f"all_databases_{timestamp}.sql"
-            command = f"mysqldump -u {username} -p{password} --all-databases > {backup_file}"
+            command = ["mysqldump", "-u", username, "-p" + password, "--all-databases"]
         else:
             backup_file = path / f"{db_name}_{timestamp}.sql"
-            command = f"mysqldump -u {username} -p{password} {db_name} > {backup_file}"
+            command = ["mysqldump", "-u", username, "-p" + password, db_name]
 
     elif db_type == "postgres":
+        backup_file = path / f"{db_name}_{timestamp}.sql"
         if db_name == "all":
-            backup_file = path / f"all_databases_{timestamp}.sql"
-            command = f"pg_dumpall -U {username} -f {backup_file}"
+            command = ["pg_dumpall", "-U", username, "-f", str(backup_file)]
         else:
-            backup_file = path / f"{db_name}_{timestamp}.sql"
-            command = f"pg_dump -U {username} -d {db_name} -f {backup_file}"
+            command = ["pg_dump", "-U", username, "-d", db_name, "-f", str(backup_file)]
+            if tablespaces:
+                command.append("--tablespaces-only")
+            if schema:
+                command.append("--schema-only")
 
     try:
-        subprocess.run(command, shell=True, check=True)
-        typer.style(f"Backup successful: {backup_file}", fg=typer.colors.GREEN, bold=True)
+        subprocess.run(command, check=True)
+        typer.style(
+            f"Backup successful: {backup_file}", fg=typer.colors.GREEN, bold=True
+        )
     except subprocess.CalledProcessError as e:
-        typer.style(f"Backup failed: {e}", fg=typer.colors.WHITE, bg=typer.colors.RED, bold=True)
+        typer.style(
+            f"Backup failed: {e}", fg=typer.colors.WHITE, bg=typer.colors.RED, bold=True
+        )
